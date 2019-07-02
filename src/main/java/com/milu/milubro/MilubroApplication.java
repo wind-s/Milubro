8c58e87861e4997d405d2be08f8cdb3c907a2975
麋鹿BRO <807424100@qq.com>
2019-07-02 18:58:14 +0800

    first time to commit

---
+++ src/main/java/com/milu/milubro/MilubroApplication.java
@@ -0,0 +1,13 @@
+package com.milu.milubro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilubroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MilubroApplication.class, args);
+    }
+
+}
